e42bae794f3b4efe33b16cd24b8bb49df71d2f47
thanhson1994 <ltsonht1994@gmail.com>
2018-03-26 00:50:14 +0700

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MinComponent } from './min/min.component';
+import { ProjectComponent } from './project/project.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MinComponent,
+    ProjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/min/min.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-min',
+  templateUrl: './min.component.html',
+  styleUrls: ['./min.component.scss']
+})
+export class MinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
